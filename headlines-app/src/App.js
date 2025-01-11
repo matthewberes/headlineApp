f35ea26fb5dc861f86da2031b150ed63c1ef1e06
matthewberes <matthewberes@trentu.ca>
2025-01-10 23:02:11 -0500

    display matches in console

--- headlines-app/src/App.js
+++ headlines-app/src/App.js
@@ -1,20 +1,35 @@
 import './App.css';
 
 function App() {
+  let input;
   let response;
   let headlines = [];
   const btnClick = () => {
-    fetch("https://newsapi.org/v2/everything?q=" + document.getElementById('text').value + "&apiKey=" + process.env.REACT_APP_API_KEY)
+    headlines = [];
+    input = document.getElementById('text').value;
+    fetch("https://newsapi.org/v2/everything?q=" + input + "&apiKey=" + process.env.REACT_APP_API_KEY)
       .then((resp) => resp.json())
       .then((data) => {
         response = data;
-        seperateTitles(data)
-      })
+        seperateTitles(data);
+      });
   }
 
   function seperateTitles(obj) {
     for (let i = 0; i < obj.articles.length; i++) {
       headlines.push(obj.articles[i].title);
+    }
+    checkMatch();
+  }
+
+  function checkMatch() {
+    for (let i = 0; i < headlines.length; i++) {
+      if (headlines[i].toLowerCase().includes(input.trim().toLowerCase())) {
+        console.log("We found a match: ");
+        console.log("Title: " + headlines[i]);
+        console.log("Author: " + response.articles[i].author);
+        console.log("Source: " + response.articles[i].source.name);
+      }
     }
   }
 
