dc2f1620c08fdfafc170a472831374642b36a314
matthewberes <matthewberes@trentu.ca>
2025-01-13 21:23:13 -0500

    print answer to div

--- headlines-app/src/components/TextInputComponent/TextInputComponent.js
+++ headlines-app/src/components/TextInputComponent/TextInputComponent.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import React from 'react';
 
 function TextInputComponent() {
   let input;
   let response;
-  let headlines = [];
-  let matches = [];
+  const [headlines, setHeadines] = useState([])
+  const [matches, setMatches] = useState([])
+  const [output, setOutput] = useState([])
+
+  // let headlines = [];
+  // let matches = headlines.map(item => <p>{item}</p>)
+  // for (let i = 0; i < headlines.length; i++) {
+  //   matches.push(headlines[i])
+  // }
   const btnClick = () => {
-    headlines = [];
+    setHeadines([]);
     input = document.getElementById('text').value;
     fetch("https://newsapi.org/v2/everything?q=" + input + "&apiKey=" + process.env.REACT_APP_API_KEY)
       .then((resp) => resp.json())
@@ -17,29 +25,34 @@ function TextInputComponent() {
   }
 
   function seperateTitles(obj) {
+    let temp = []
     for (let i = 0; i < obj.articles.length; i++) {
-      headlines.push(obj.articles[i].title);
+      temp.push(obj.articles[i].title);
     }
+    setHeadines(temp)
     checkMatch();
   }
 
   function checkMatch() {
-    matches = [];
+    setMatches([]);
+    let temp = []
     for (let i = 0; i < headlines.length; i++) {
       if (headlines[i].toLowerCase().includes(input.trim().toLowerCase())) {
-        matches.push(response.articles[i]);
+        temp.push(response.articles[i]);
         console.log("We found a match: ");
         console.log("Title: " + headlines[i]);
         console.log("Author: " + response.articles[i].author);
         console.log("Source: " + response.articles[i].source.name);
       }
     }
+    setMatches(temp)
+    setOutput(matches.map(item => <p>{item.title}</p>))
   }
   return (
     <div>
       <input type="input" id="text"></input>
       <button onClick={btnClick}>Submit</button>
-      {matches.map(item => <p>key={item.title} name={item.author}</p>)}
+      {output}
     </div>
   );
 }
