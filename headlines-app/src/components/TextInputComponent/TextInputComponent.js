caa50b933b53c5bdd9100e014aaa2abb9a73a0d4
matthewberes <matthewberes@trentu.ca>
2025-01-14 18:18:07 -0500

    format output a bit better, fix state bug

--- headlines-app/src/components/TextInputComponent/TextInputComponent.js
+++ headlines-app/src/components/TextInputComponent/TextInputComponent.js
@@ -4,17 +4,13 @@ import React from 'react';
 function TextInputComponent() {
   let input;
   let response;
-  const [headlines, setHeadines] = useState([])
-  const [matches, setMatches] = useState([])
+  let headliner = [];
+  let matchy = [];
+  // const [headlines, setHeadines] = useState([])
+  // const [matches, setMatches] = useState([])
   const [output, setOutput] = useState([])
 
-  // let headlines = [];
-  // let matches = headlines.map(item => <p>{item}</p>)
-  // for (let i = 0; i < headlines.length; i++) {
-  //   matches.push(headlines[i])
-  // }
   const btnClick = () => {
-    setHeadines([]);
     input = document.getElementById('text').value;
     fetch("https://newsapi.org/v2/everything?q=" + input + "&apiKey=" + process.env.REACT_APP_API_KEY)
       .then((resp) => resp.json())
@@ -25,33 +21,33 @@ function TextInputComponent() {
   }
 
   function seperateTitles(obj) {
-    let temp = []
+    headliner = []
     for (let i = 0; i < obj.articles.length; i++) {
-      temp.push(obj.articles[i].title);
+      headliner.push(obj.articles[i].title);
     }
-    setHeadines(temp)
     checkMatch();
   }
 
   function checkMatch() {
-    setMatches([]);
-    let temp = []
-    for (let i = 0; i < headlines.length; i++) {
-      if (headlines[i].toLowerCase().includes(input.trim().toLowerCase())) {
-        temp.push(response.articles[i]);
+    matchy = []
+    for (let i = 0; i < headliner.length; i++) {
+      if (headliner[i].toLowerCase().includes(input.trim().toLowerCase())) {
+        matchy.push(response.articles[i]);
         console.log("We found a match: ");
-        console.log("Title: " + headlines[i]);
+        console.log("Title: " + headliner[i]);
         console.log("Author: " + response.articles[i].author);
         console.log("Source: " + response.articles[i].source.name);
       }
     }
-    setMatches(temp)
-    setOutput(matches.map(item => <p>{item.title}</p>))
+    setOutput(matchy.map(item => <p><a href={item.url}>{item.source.name}</a></p>))
   }
+
+  let testy = <p>{output.length} matches found:</p>
   return (
     <div>
       <input type="input" id="text"></input>
       <button onClick={btnClick}>Submit</button>
+      {output.length > 0 && testy}
       {output}
     </div>
   );
